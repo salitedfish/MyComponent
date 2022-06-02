41c406980ff75c062d2157d88ea45fde6708018c
salitedfish <1525185228@qq.com>
2022-06-02 16:42:49 +0800

    update

--- src/util/test.ts
+++
@@ -1,8 +0,0 @@
-import { useDebounce } from "./index";
-import { useDebounceFn } from "@vueuse/core";
-const handle = (a: number, b: number) => {};
-const test = useDebounce(handle, 2000);
-
-const test1 = useDebounceFn(handle);
-test(1, 1);
-test1(3, 5);
--- src/utility/获取上传进度.ts
+++ src/utility/获取上传进度.ts
@@ -1,11 +1,15 @@
-const service = (callBack) => {
-    axios.post(this.router.uploadCopyrightFile.path, {}, {
-        headers: {
-          Authorization: '',
-          'Content-Type': 'application/json',
-        }, 
-        onUploadProgress: (p)=>{
-          if(callBack) callBack(p)
-        }
-    })
-}
+const service = callBack => {
+  axios.post(
+    this.router.uploadCopyrightFile.path,
+    {},
+    {
+      headers: {
+        Authorization: "",
+        "Content-Type": "application/json"
+      },
+      onUploadProgress: p => {
+        if (callBack) callBack(p);
+      }
+    }
+  );
+};
